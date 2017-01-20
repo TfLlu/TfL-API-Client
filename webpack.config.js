3c912c5752bd556f45f42b9ea94e6f5afb57b3af
Daniel Duton <daniel@duton.lu>
2017-01-20 12:10:12 +0100

    init webpack and copy client from api

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path              = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const ROOT_DIR = __dirname;
+const SRC_DIR  = path.resolve(__dirname, 'src');
+const HTML_DIR = path.resolve(__dirname, 'html');
+const DIST_DIR = path.resolve(__dirname, 'dist');
+
+module.exports = {
+    entry: path.resolve(SRC_DIR, 'client.js'),
+    output: {
+        path: DIST_DIR,
+        filename: 'bundle.js',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                include: ROOT_DIR
+            }
+        ]
+    },
+    plugins: [
+        // Simply copies the files over
+        new CopyWebpackPlugin([
+            { from: HTML_DIR } // to: output.path
+        ]),
+    ],
+    // Create Sourcemaps for the bundle
+    devtool: 'source-map',
+    devServer: {
+        contentBase: DIST_DIR,
+    }
+};
